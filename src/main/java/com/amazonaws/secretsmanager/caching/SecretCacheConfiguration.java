47861e2e020d7ba51d37398974245bc1d3f40083
Caitlin Russell <73604262+crus-umich@users.noreply.github.com>
2025-06-16 13:03:30 -0700

    Decreasing default sleep time for refreshNow() calls, sleep time now … (#160)
    *Issue #, if available:*
    https://github.com/aws/aws-secretsmanager-caching-java/issues/154
    
    *Description of changes:* Decreased default sleep time for refreshNow()
    calls to be between 50 and 100 ms, also made this value configurable
    
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache 2.0 license.
    
    ---------
    
    Co-authored-by: Caitlin Russell <caitarus@amazon.com>

--- src/main/java/com/amazonaws/secretsmanager/caching/SecretCacheConfiguration.java
+++ src/main/java/com/amazonaws/secretsmanager/caching/SecretCacheConfiguration.java
@@ -34,6 +34,12 @@ public class SecretCacheConfiguration {
     /** The default version stage to use when retrieving secret values. */
     public static final String DEFAULT_VERSION_STAGE = "AWSCURRENT";
 
+    /** 
+     * The default maximum jitter value in milliseconds to use when forcing a refresh.
+     * This prevents continuous refreshNow() calls by adding a random sleep.
+     */
+    public static final long DEFAULT_FORCE_REFRESH_JITTER = 100;
+
     /** The client this cache instance will use for accessing AWS Secrets Manager. */
     private SecretsManagerClient client = null;
 
@@ -59,6 +65,13 @@ public class SecretCacheConfiguration {
      * this cache.
      */
     private String versionStage = DEFAULT_VERSION_STAGE;
+
+    /**
+     * When forcing a refresh using the refreshNow method, a random sleep
+     * will be performed using this value.  This helps prevent code from
+     * executing a refreshNow in a continuous loop without waiting.
+     */
+    private long forceRefreshJitterMillis = DEFAULT_FORCE_REFRESH_JITTER;
 
     /**
      * Default constructor for the SecretCacheConfiguration object.
@@ -234,6 +247,47 @@ public class SecretCacheConfiguration {
      */
     public SecretCacheConfiguration withVersionStage(String versionStage) {
         this.setVersionStage(versionStage);
+        return this;
+    }
+
+    /**
+     * Returns the refresh jitter that is used when force refreshing secrets.
+     *
+     * @return The maximum jitter sleep time in milliseconds used with refreshing secrets.
+     */
+    public long getForceRefreshJitterMillis() {
+        return this.forceRefreshJitterMillis;
+    }
+
+    /**
+     * Sets the maximum sleep time in milliseconds between force refresh calls.
+     * This value is used to prevent continuous refreshNow() calls in tight loops
+     * by adding a random sleep between half the configured value and the full value.
+     * The value must be greater than or equal to zero.
+     *
+     * @param forceRefreshJitterMillis
+     *            The maximum sleep time in milliseconds between force refresh calls.
+     * @throws IllegalArgumentException if the value is negative
+     */
+    public void setForceRefreshJitterMillis(long forceRefreshJitterMillis) {
+        if (forceRefreshJitterMillis < 0) {
+            throw new IllegalArgumentException("Force refresh jitter must be greater than or equal to zero");
+        }
+        this.forceRefreshJitterMillis = forceRefreshJitterMillis;
+    }
+
+    /**
+     * Sets the maximum sleep time in milliseconds between force refresh calls.
+     * This value is used to prevent continuous refreshNow() calls in tight loops
+     * by adding a random sleep between half the configured value and the full value.
+     *
+     * @param forceRefreshJitterMillis
+     *            The maximum sleep time in milliseconds between force refresh calls.
+     * @return The updated ClientConfiguration object with the new refresh sleep time.
+     * @throws IllegalArgumentException if the value is negative
+     */
+    public SecretCacheConfiguration withForceRefreshJitterMillis(long forceRefreshJitterMillis) {
+        this.setForceRefreshJitterMillis(forceRefreshJitterMillis);
         return this;
     }
 
